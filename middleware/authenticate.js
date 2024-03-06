e1e5df0c1d373802e72a66bf830b5488f2796df5
John McCants <johnmccants@JohnsMacStudio.attlocal.net>
2024-03-05 20:23:54 -0800

    Authenticate working

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const supabase = require("../supabaseClient");
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  const token = authHeader?.split(" ")[1]; // Expecting "Bearer [token]"
-
+  const token = authHeader?.split(" ")[1]; // Expecting Bearer [token]
   console.log(token, "THIS IS THE TOKEN");
-
   if (!token) {
     return res
       .status(401)
@@ -15,11 +18,12 @@ const authenticate = async (req, res, next) => {
   }
 
   // Note: Setting up Supabase client to include the Authorization header with the token
-  supabase.auth.setAuth(token);
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
   try {
-    const { data: user, error } = await supabase.auth.getUser();
-
+    const { data: user, error } = await supabase.auth.user();
     if (error) throw new Error(error.message);
 
     // Attaching the user object to the request so it can be used in subsequent routes
